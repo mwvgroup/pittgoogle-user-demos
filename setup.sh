9d93f3af7f28a958dc68543c48bf8860d733253a
Chris Hernandez <hernandezchristopher.13@gmail.com>
2023-01-11 15:47:33 -0500

    rearranging sections

--- setup.sh
+++ setup.sh
@@ -8,10 +8,12 @@ survey="${3:-elasticc}"
 PROJECT_ID="${GOOGLE_CLOUD_PROJECT}"
 
 # GCP resources used in this script
-pubsub_trigger_topic="${survey}-alerts"
-pubsub_SuperNNova_topic="${survey}-SuperNNova"
-bq_dataset="${PROJECT_ID}:${survey}_alerts"
 alerts_table="SuperNNova"
+bq_dataset="${PROJECT_ID}:${survey}_alerts"
+pubsub_SuperNNova_topic="${survey}-SuperNNova"
+pubsub_trigger_topic="${survey}-alerts"
+route="/"
+runinvoker_svcact="cloud-run-invoker@${GOOGLE_CLOUD_PROJECT}.iam.gserviceaccount.com"
 subscrip="elasticc-loop" #used to trigger Cloud Run module
 topic="elasticc-loop"
 topic_project="avid-heading-329016"
@@ -34,37 +36,33 @@ if [ "${teardown}" != "True" ]; then
     bq mk --dataset "${bq_dataset}"
     bq mk --table "${bq_dataset}.${alerts_table}" "templates/bq_${survey}_${alerts_table}_schema.json"
 
+    # Deploy Cloud Run
+    echo "Creating container image and deploying to Cloud Run..."
+    moduledir="classifier"  # assumes we're in the repo's root dir
+    config="${moduledir}/cloudbuild.yaml"
+    # gcloud builds submit --config="${config}" \
+    #     --substitutions=_SURVEY="${survey}",_TESTID="${testid}" \
+    #     "${moduledir}"
+    url=$(gcloud builds submit --config="${config}" \
+        --substitutions=_SURVEY="${survey}",_TESTID="${testid}" \
+        "${moduledir}" | grep "Service [^ ]* is running" | awk '{print $2}')
+    
+    echo "Creating trigger subscription for Cloud Run"
+    gcloud pubsub subscriptions create "${subscrip}" \
+        --topic "${topic}" \
+        --topic-project "${topic_project}" \
+        --ack-deadline=600 \
+        --push-endpoint="${url}${route}" \
+        --push-auth-service-account="${runinvoker_svcact}"
+
 else
     # ensure that we do not teardown production resources
     if [ "${testid}" != "False" ]; then
         echo "Removing BigQuery, Pub/Sub resources for Cloud Run..."
         gcloud pubsub topics delete "${pubsub_trigger_topic}"
         gcloud pubsub topics delete "${pubsub_SuperNNova_topic}"
-        gcloud pubsub subscriptions delete "${subscrip}"
+        gcloud pubsub subscriptions delete "${subscrip}" #needed to stop the Cloud Run module
         bq rm --table "${bq_dataset}.${alerts_table}"
         bq rm --dataset true "${bq_dataset}"
     fi
 fi
-
-# Deploy Cloud Run
-echo "Creating container image and deploying to Cloud Run..."
-moduledir="classifier"  # assumes we're in the repo's root dir
-config="${moduledir}/cloudbuild.yaml"
-gcloud builds submit --config="${config}" \
-    --substitutions=_SURVEY="${survey}",_TESTID="${testid}" \
-    "${moduledir}"
-
-# the URL must be manually copy-pasted into the following code in order to create the subscription (we need to automate this)
-# url="<copy-paste the Cloud Run URL here>"
-# route="/"
-# runinvoker_svcact="cloud-run-invoker@${GOOGLE_CLOUD_PROJECT}.iam.gserviceaccount.com"
-
-# gcloud pubsub subscriptions create "${subscrip}" \
-#     --topic "${topic}" \
-#     --topic-project "${topic_project}" \
-#     --ack-deadline=600 \
-#     --push-endpoint="${url}${route}" \
-#     --push-auth-service-account="${runinvoker_svcact}"
-
-#to stop the Cloud Run module, you must delete the trigger subscription:
-# gcloud pubsub subscriptions delete "${subscrip}"
