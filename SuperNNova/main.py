979866e808aa255b834db665954ae76049b9d6ac
troyraen <troy.raen@pitt.edu>
2023-09-28 02:31:23 -0700

    refactor to use pittgoogle-client

--- SuperNNova/main.py
+++ SuperNNova/main.py
@@ -3,206 +3,159 @@
 
 """Classify alerts using SuperNNova (M¨oller & de Boissi`ere 2019)."""
 
-import base64
-from datetime import datetime, timezone
-import io
 import os
+from datetime import datetime, timezone
+from pathlib import Path
 
-from google.cloud import bigquery, logging
-import json
-import fastavro
-
+import flask
+import google.cloud.logging
 import numpy as np
 import pandas as pd
 import pittgoogle
-from pathlib import Path
 from supernnova.validation.validate_onthefly import classify_lcs
 
-from flask import Flask, request
+# connect the python logger to the google cloud logger
+# by default, this captures INFO level and above
+# pittgoogle uses the python logger
+# we don't currently use the python logger directly in this script, but we could
+# [TODO] make sure this is actually working
+google.cloud.logging.Client().setup_logging()
 
 PROJECT_ID = os.getenv("GCP_PROJECT")
 TESTID = os.getenv("TESTID")
 SURVEY = os.getenv("SURVEY")
 
-# specify module version
-brokerVersion = "v0.7"
-
-# connect to the logger
-logging_client = logging.Client()
-log_name = "classify-snn-cloudrun"  # same log for all broker instances
-logger = logging_client.logger(log_name)
-
-# GCP resources used in this module
-bq_dataset = f"{SURVEY}_alerts"
-ps_topic = f"{SURVEY}-SuperNNova"
-if TESTID != "False":  # attach the testid to the names
-    bq_dataset = f"{bq_dataset}_{TESTID}"
-    ps_topic = f"{ps_topic}-{TESTID}"
-bq_table_id = f"{PROJECT_ID}.{bq_dataset}.SuperNNova"
-
-schema_out = fastavro.schema.load_schema("elasticc.v0_9_1.brokerClassfication.avsc")
-workingdir = Path(__file__).resolve().parent
-schema_map = load_schema_map(SURVEY, TESTID, schema=(workingdir / f"{SURVEY}-schema-map.yml"))
-alert_ids = AlertIds(schema_map)
-id_keys = alert_ids.id_keys
-if SURVEY == "elasticc":
-    schema_in = "elasticc.v0_9_1.alert.avsc"
-else:
-    schema_in = None
-
+# classifier
+CLASSIFIER_NAME = "supernnova"
 model_dir_name = "ZTF_DMAM_V19_NoC_SNIa_vs_CC_forFink"
 model_file_name = "vanilla_S_0_CLF_2_R_none_photometry_DF_1.0_N_global_lstm_32x2_0.05_128_True_mean.pt"
-model_path = Path(__file__).resolve().parent / f"{model_dir_name}/{model_file_name}"
-bq_client = bigquery.Client(project=PROJECT_ID)
-bq_schema = [
-    bigquery.SchemaField("alertId", "INTEGER"),
-    bigquery.SchemaField("diaObjectId", "INTEGER"),
-    bigquery.SchemaField("diaSourceId", "INTEGER"),
-    bigquery.SchemaField("prob_class0", "FLOAT"),
-    bigquery.SchemaField("prob_class1", "FLOAT"),
-    bigquery.SchemaField("predicted_class", "INTEGER"),
-    bigquery.SchemaField("brokerVersion", "STRING"),
-    bigquery.SchemaField("elasticcPublishTimestamp", "TIMESTAMP"),
-    bigquery.SchemaField("brokerIngestTimestamp", "TIMESTAMP"),
-    bigquery.SchemaField("classifierTimestamp", "TIMESTAMP")
-]
+MODEL_PATH = Path(__file__).resolve().parent / model_dir_name / model_file_name
 
-app = Flask(__name__)
+# incoming
+SCHEMA_IN = "elasticc.v0_9_1.alert"  # view the schema: pittgoogle.Schemas.get(SCHEMA_IN).avsc
+
+# outgoing
+HTTP_204 = 204  # http code: success (no content)
+HTTP_400 = 400  # http code: bad request
+SCHEMA_OUT = "elasticc.v0_9_1.brokerClassification"  # view the schema: pittgoogle.Schemas.get(SCHEMA_OUT).avsc
+TABLE = pittgoogle.Table.from_cloud(CLASSIFIER_NAME, survey=SURVEY, testid=TESTID)
+TOPIC = pittgoogle.Topic.from_cloud("SuperNNova", survey=SURVEY, testid=TESTID, projectid=PROJECT_ID)
+
+
+app = flask.Flask(__name__)
+
+
 @app.route("/", methods=["POST"])
 def index():
-    """Classify alert with SuperNNova; publish and store results.
+    """Classify alert; publish and store results.
 
     This function is intended to be triggered by Pub/Sub messages, via Cloud Run.
     """
-    envelope = request.get_json()
+    # the module runs on Cloud Run as an HTTP endpoint
+    # extract the envelope from the request that triggered the endpoint
+    # this contains a single Pub/Sub message with the alert to be processed
+    envelope = flask.request.get_json()
 
-    # do some checks
-    if not envelope:
-        msg = "no Pub/Sub message received"
-        print(f"error: {msg}")
-        return f"Bad Request: {msg}", 400
-
-    if not isinstance(envelope, dict) or "message" not in envelope:
-        msg = "invalid Pub/Sub message format"
-        print(f"error: {msg}")
-        return f"Bad Request: {msg}", 400
-
-    # unpack the alert
-    msg = envelope["message"]
-
-    alert_dict = open_alert(msg["data"], load_schema=schema_in)
-    a_ids = alert_ids.extract_ids(alert_dict=alert_dict)
-    
+    # unpack the alert. raises a `BadRequest` if the envelope does not contain a valid message
     try:
-        publish_time = datetime.strptime(msg["publish_time"].replace("Z","+00:00"), '%Y-%m-%dT%H:%M:%S.%f%z')
-    except ValueError:
-        publish_time = datetime.strptime(msg["publish_time"].replace("Z","+00:00"), '%Y-%m-%dT%H:%M:%S%z')
-
-    attrs = {
-        **msg["attributes"],
-        "brokerIngestTimestamp": publish_time,
-        id_keys.objectId: str(a_ids.objectId),
-        id_keys.sourceId: str(a_ids.sourceId),
-    }
+        alert = pittgoogle.Alert.from_cloud_run(envelope=envelope, schema_name=SCHEMA_IN)
+    except pittgoogle.exceptions.BadRequest as exc:
+        return str(exc), HTTP_400
 
     # classify
-    snn_dict = _classify_with_snn(alert_dict, attrs)
+    classifications = _classify(alert)
 
-    # create the message for elasticc and publish the stream
-    avro = _create_elasticc_msg(dict(alert=alert_dict, SuperNNova=snn_dict), attrs)
-    gcp_utils.publish_pubsub(ps_topic, avro, attrs=attrs)
+    # publish
+    TOPIC.publish(_create_outgoing_alert(alert, classifications))
+    TABLE.insert_rows([classifications])
 
-    # write to BigQuery
-    errors = bq_client.insert_rows(bq_table_id, [snn_dict], bq_schema)
-    if len(errors) > 0:
-        logger.log_text(f"BigQuery insert error: {errors}", severity="WARNING")
+    return "", HTTP_204
 
-    return ("", 204)
 
-def _classify_with_snn(alert_dict: dict, attrs: dict) -> dict:
+def _classify(alert: pittgoogle.Alert) -> dict:
     """Classify the alert using SuperNNova."""
     # init
-    snn_df = _format_for_snn(alert_dict)
+    snn_df = _format_for_classifier(alert)
     device = "cpu"
 
     # classify
-    _, pred_probs = classify_lcs(snn_df, model_path, device)
+    _, pred_probs = classify_lcs(snn_df, MODEL_PATH, device)
 
-    # extract results to dict and attach alert/object/source ids.
-    # use `.item()` to convert numpy -> python types for later json serialization
+    # extract results to a dict that matches the TABLE schema (TABLE.table.schema)
+    # use `.item()` to convert numpy -> python types for later serialization
     pred_probs = pred_probs.flatten()
-    snn_dict = {
-        "alertId": int(alert_dict["alertId"]),
-        id_keys.objectId: snn_df.objectId,
-        id_keys.sourceId: snn_df.sourceId,
+    classifications = {
+        "alertId": alert.alertid,
+        "diaObjectId": alert.objectid,
+        "diaSourceId": alert.sourceid,
         "prob_class0": pred_probs[0].item(),
         "prob_class1": pred_probs[1].item(),
         "predicted_class": np.argmax(pred_probs).item(),
-        "brokerVersion": brokerVersion,
-        "elasticcPublishTimestamp": int(attrs["kafka.timestamp"])/1000,
-        "brokerIngestTimestamp": attrs["brokerIngestTimestamp"],
-        "classifierTimestamp": datetime.now(timezone.utc)
+        "timestamp": datetime.now(timezone.utc),
     }
 
-    return snn_dict
+    return classifications
 
 
-def _format_for_snn(alert_dict: dict) -> pd.DataFrame:
-    """Compute features and cast to a DataFrame for input to SuperNNova."""
-    # cast alert to dataframe
-    alert_df = data_utils.alert_dict_to_dataframe(alert_dict, schema_map)
-
-    # start a dataframe for input to SNN
-    snn_df = pd.DataFrame(data={"SNID": alert_df.objectId}, index=alert_df.index)
-    snn_df.objectId = alert_df.objectId
-    snn_df.sourceId = alert_df.sourceId
-    snn_df["FLT"] = alert_df["filterName"]
-    snn_df["FLUXCAL"] = alert_df["psFlux"]
-    snn_df["FLUXCALERR"] = alert_df["psFluxErr"]
-    snn_df["MJD"] = alert_df["midPointTai"]
-
+def _format_for_classifier(alert: pittgoogle.Alert) -> pd.DataFrame:
+    """Create a DataFrame for input to SuperNNova."""
+    alert_df = alert.dataframe
+    snn_df = pd.DataFrame(
+        data={
+            # select a subset of columns and rename them for SuperNNova
+            # get_key returns the name that the survey uses for a given field
+            # for the full mapping, see alert.schema.map
+            "FLT": alert_df[alert.get_key("filter")],
+            "FLUXCAL": alert_df[alert.get_key("flux")],
+            "FLUXCALERR": alert_df[alert.get_key("fluxerr")],
+            "MJD": alert_df[alert.get_key("mjd")],
+            # add the object ID
+            "SNID": [alert.objectid] * len(alert_df.index),
+        },
+        index=alert_df.index,
+    )
     return snn_df
 
 
-def _create_elasticc_msg(alert_dict, attrs):
-    """Create a message according to the ELAsTiCC broker classifications schema.
-    https://github.com/LSSTDESC/plasticc_alerts/blob/main/Examples/plasticc_schema
-    """
-    # original elasticc alert as a dict
-    elasticc_alert = alert_dict["alert"]
-    supernnova_results = alert_dict["SuperNNova"]
+def _create_outgoing_alert(alert_in: pittgoogle.Alert, results: dict) -> pittgoogle.Alert:
+    """Combine the incoming alert with the classification results to create the outgoing alert."""
+    # need to convert the broker ingest timestamp to conform with SCHEMA_OUT
+    # occasionally a Pub/Sub timestamp doesn't include microseconds, so we need a try/except
+    broker_ingest_time = alert_in.msg.publish_time.replace("Z", "+00:00")
+    try:
+        broker_ingest_time = datetime.strptime(broker_ingest_time, "%Y-%m-%dT%H:%M:%S.%f%z")
+    except ValueError:
+        broker_ingest_time = datetime.strptime(broker_ingest_time, "%Y-%m-%dT%H:%M:%S%z")
 
-    # here are a few things you'll need
-    elasticcPublishTimestamp = int(attrs["kafka.timestamp"])
-    brokerIngestTimestamp = attrs.pop("brokerIngestTimestamp")
-
+    # write down the mappings between our classifications and the ELASTTIC taxonomy
+    # https://github.com/LSSTDESC/elasticc/blob/main/taxonomy/taxonomy.ipynb
     classifications = [
-        {
-            "classId": 2222,
-            "probability": supernnova_results["prob_class0"],
-        },
+        {"classId": 2222, "probability": results["prob_class0"]},
     ]
 
-    msg = {
-        "alertId": elasticc_alert["alertId"],
-        "diaSourceId": get_value("sourceId", elasticc_alert, schema_map),
-        "elasticcPublishTimestamp": elasticcPublishTimestamp,
-        "brokerIngestTimestamp": brokerIngestTimestamp,
+    # construct a dict that conforms to SCHEMA_OUT
+    outgoing_dict = {
+        "alertId": alert_in.alertid,
+        "diaSourceId": alert_in.sourceid,
+        "elasticcPublishTimestamp": int(alert_in.attributes["kafka.timestamp"]),
+        "brokerIngestTimestamp": broker_ingest_time,
         "brokerName": "Pitt-Google Broker",
-        "brokerVersion": brokerVersion,
+        "brokerVersion": "v0.6",
         "classifierName": "SuperNNova_v1.3",
-        "classifierParams": "",  # leave this blank for now
-        "classifications": classifications
+        "classifierParams": MODEL_PATH,  # record the training file
+        "classifications": classifications,
     }
 
-    # avro serialize the dictionary
-    avro = _dict_to_avro(msg, schema_out)
-    return avro
+    # create the outgoing Alert
+    # typically the pitt-google broker adds the IDs to the alert attributes
+    # however, we're receiving alerts directly from the broker's consumer so the IDs are not yet attached
+    # let's add them. these are not currently used but may help downstream users.
+    alert_in.add_id_attributes()
+    alert_out = pittgoogle.Alert.from_dict(
+        payload=outgoing_dict, attributes=alert_in.attributes, schema_name=SCHEMA_OUT
+    )
+    # also add the predicted class to the attributes
+    # again, not currently used, but is good practice and may help downstream users
+    alert_out.attributes[CLASSIFIER_NAME] = classifications["predicted_class"]
 
-def _dict_to_avro(msg: dict, schema: dict):
-    """Avro serialize a dictionary."""
-    fout = io.BytesIO()
-    fastavro.schemaless_writer(fout, schema, msg)
-    fout.seek(0)
-    avro = fout.getvalue()
-    return avro
+    return alert_out
