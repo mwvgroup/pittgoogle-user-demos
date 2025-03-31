8c9068b8fd19f3345d313f8f20b1b5fa419d8348
Chris Hernandez <hernandezchristopher.13@gmail.com>
2025-03-31 12:32:34 -0400

    update main.py

--- cloud-run/filtering/main.py
+++ cloud-run/filtering/main.py
@@ -1,10 +1,15 @@
 #!/usr/bin/env python3
 # -*- coding: UTF-8 -*-
 
-"""This module filters the LSST alert stream to discover intra-night, never-before-seen transients."""
+"""This module filters the LSST alert stream to discover never-before-seen transients."""
 
+import os
 import flask
 import pittgoogle
+
+PROJECT_ID = os.getenv("GCP_PROJECT")
+TESTID = os.getenv("TESTID")
+SURVEY = os.getenv("SURVEY")
 
 # Variables for incoming data
 # A url route is used in setup.sh when the trigger subscription is created.
@@ -14,6 +19,9 @@ ROUTE_RUN = "/"  # HTTP route that will trigger run(). Must match setup.sh
 # Variables for outgoing data
 HTTP_204 = 204  # HTTP code: Success
 HTTP_400 = 400  # HTTP code: Bad Request
+TOPIC_FIRST_DETECTION = pittgoogle.Topic.from_cloud(
+    "first-detection", survey=SURVEY, testid=TESTID, projectid=PROJECT_ID
+)
 
 app = flask.Flask(__name__)
 
@@ -35,8 +43,25 @@ def run():
     except pittgoogle.exceptions.BadRequest as exc:
         return str(exc), HTTP_400
 
-    _filter(alert)
+    filter_alert(alert)
 
 
-def _filter(alert: pittgoogle.Alert) -> pittgoogle.Alert:
-    return alert
+def filter_alert(alert: pittgoogle.Alert):
+    """Filters the LSST alert stream to identify never-before-seen non-ssObject transients.
+    Discoveries are published to a Pub/Sub topic.
+    """
+    if not alert.get("ssobjectid") and not alert.get("prv_sources"):
+        TOPIC_FIRST_DETECTION.publish(create_outgoing_alert(alert), serializer="json")
+    return "", HTTP_204
+
+
+def create_outgoing_alert(alert: pittgoogle.Alert) -> pittgoogle.Alert:
+    """Creates a new Alert object."""
+    outgoing_msg = {
+        alert.get_key("objectid"): alert.get("objectid"),
+        alert.get_key("ra"): alert.get("ra"),
+        alert.get_key("dec"): alert.get("dec"),
+        alert.get_key("mjd"): alert.get("mjd"),
+    }
+    attrs = {"first_detection": "True"}
+    return pittgoogle.alert.Alert(outgoing_msg, attributes=attrs)
